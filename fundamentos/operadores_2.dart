16e50901b554639057c4d11be2e3cbd396972e4d
Wisley César <wisleycesar32@gmail.com>
2024-01-08 18:55:25 -0300

    fundamentos

---
+++ fundamentos/operadores_2.dart
@@ -0,0 +1,27 @@
+main() {
+  // Operadores de atribuição (binário/ infix).
+  double a = 2;
+  a += 3;
+  a -= 3;
+  a *= 5;
+  a /= 5;
+  a %= 2;
+
+  print(a);
+
+  // Operadores Relacional (binário/ infix) --> o resultado sempre será BOLL
+
+  print(3 > 2);
+  print(3 >= 2);
+  print(3 < 2);
+  print(3 != 3);
+  print(3 == 3);
+  print(3 == '3');
+
+  print(4 + 7 + 5 > 10 && 5 + 7 + 9 != 5 - 7 - 9);
+
+  // 101 = 5
+  // 100 = 4
+  // 100 = 4
+  print(5 & 4);
+}
